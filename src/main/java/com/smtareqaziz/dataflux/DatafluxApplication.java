f4fe99936a066bdfff2a73b508001b7d790a2699
S M Tareq Aziz <smtareqaziz007@gmail.com>
2024-11-23 19:53:31 +0600

    Project Bootstrapped

---
+++ src/main/java/com/smtareqaziz/dataflux/DatafluxApplication.java
@@ -0,0 +1,13 @@
+package com.smtareqaziz.dataflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatafluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatafluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smtareqaziz/dataflux/DatafluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smtareqaziz.dataflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatafluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
